b79de4413119a1db08173cabf3c5a8bfa2bda2b9
NeshGogo <rafaelaguero10@hotmail.com>
2021-05-29 10:53:08 -0400

    adding gender entity and configuring entity framework

---
+++ MovieTheater/Controllers/GendersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieTheater.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTheater.Controllers
+{
+    [ApiController]
+    [Route("api/genders")]
+    public class GendersController : ControllerBase
+    {
+        private readonly MovieTheaterDbContext context;
+
+        public GendersController(MovieTheaterDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Gender>>> Get()
+        {
+            var results = await context.Genders.ToListAsync();
+            return results;
+        }
+
+    }
+}
---
+++ MovieTheater/MovieTheaterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTheater.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTheater
+{
+    public class MovieTheaterDbContext : DbContext
+    {
+        public MovieTheaterDbContext( DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Gender> Genders { get; set; }
+    }
+}
