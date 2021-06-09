3ceae8d7f0a35a99f4d72c024f83ff3b85735b1d
NeshGogo <rafaelaguero10@hotmail.com>
2021-06-09 16:50:12 -0400

    adding test project, InMemoryDataBase and baseTests class.

---
+++ MovieTheater.Test/BaseTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MovieTheater.Helpers;
+using NetTopologySuite;
+
+namespace MovieTheater.Test
+{
+    public class BaseTests
+    {
+        protected MovieTheaterDbContext BuildContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<MovieTheaterDbContext>()
+                .UseInMemoryDatabase(dbName).Options;
+            return new MovieTheaterDbContext(options);
+        }
+
+        protected IMapper AutoMapperConfigure()
+        {
+            var config = new MapperConfiguration(options =>
+           {
+               var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+               options.AddProfile(new AutoMapperProfiles(geometryFactory));
+           });
+            return config.CreateMapper();
+        }
+    }
+}
