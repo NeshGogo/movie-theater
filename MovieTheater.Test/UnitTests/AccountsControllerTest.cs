8ebf46e600e9206b99fab3828296ea40ea5f22f1
NeshGogo <rafaelaguero10@hotmail.com>
2021-06-10 20:43:13 -0400

    Creating tests to  moviescontroller and accountControllers

---
+++ MovieTheater.Test/UnitTests/AccountsControllerTest.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MovieTheater.Controllers;
+using MovieTheater.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTheater.Test.UnitTests
+{
+    [TestClass]
+    public class AccountsControllerTest : BaseTests
+    {
+        [TestMethod]
+        public async Task CreateUser()
+        {
+            var dbName = Guid.NewGuid().ToString();
+            await CreateUser(dbName);
+            var context2 = BuildContext(dbName);
+            var count = await context2.Users.CountAsync();
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        public async Task UserCantLoging()
+        {
+            var dbName = Guid.NewGuid().ToString();
+            await CreateUser(dbName);
+            var controller = BuildAccountsController(dbName);
+            var user = new UserInfoDTO() { Email = "test@test.com", Password = "badPassowrd" };
+            var response = await controller.Login(user);
+            Assert.IsNull(response.Value);
+            var result = response.Result as BadRequestObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.StatusCode, StatusCodes.Status400BadRequest);
+        }
+
+        [TestMethod]
+        public async Task UserCanLoging()
+        {
+            var dbName = Guid.NewGuid().ToString();
+            await CreateUser(dbName);
+            var controller = BuildAccountsController(dbName);
+            var user = new UserInfoDTO() { Email = "test@test.com", Password = "Aa123456*" };
+            var response = await controller.Login(user);
+            Assert.IsNotNull(response.Value);
+            Assert.IsNotNull(response.Value.Token);
+        }
+
+        private async Task CreateUser(string dbName)
+        {
+            var controller = BuildAccountsController(dbName);
+            var user = new UserInfoDTO() { Email = "test@test.com", Password = "Aa123456*" };
+            await controller.CreateUser(user);
+        }
+
+        private AccountsController BuildAccountsController(string dbName)
+        {
+            var context = BuildContext(dbName);
+            var myUserStore = new UserStore<IdentityUser>(context);
+            var userManager = BuildUserManager<IdentityUser>(myUserStore);
+            var mapper = ConfigureAutoMapper();
+            var httpContext = new DefaultHttpContext();
+            MockAuth(httpContext);
+            var signInManager = SetupSignInManager(userManager, httpContext);
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"jwt:key", "Klw449hzuEkbKyJMdjlOeDmudu7XjCBiV8IVb3COLDwT7u1cbsANDocOID2A037QXYkWhhP6qzEuF9cDbLPhQUxSK6m1AGtA6WAw" }
+            };
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+            return new AccountsController(context, mapper, userManager, signInManager, configuration);
+        }
+        private  Mock<IAuthenticationService> MockAuth(HttpContext context)
+        {
+            var auth = new Mock<IAuthenticationService>();
+            context.RequestServices = new ServiceCollection().AddSingleton(auth.Object).BuildServiceProvider();
+            return auth;
+        }
+        // Source: https://github.com/dotnet/aspnetcore/blob/master/src/Identity/test/Shared/MockHelpers.cs
+        // Source: https://github.com/dotnet/aspnetcore/blob/master/src/Identity/test/Identity.Test/SignInManagerTest.cs
+        // Some code was modified to be adapted to our project.
+
+        private UserManager<TUser> BuildUserManager<TUser>(IUserStore<TUser> store = null) where TUser : class
+        {
+            store = store ?? new Mock<IUserStore<TUser>>().Object;
+            var options = new Mock<IOptions<IdentityOptions>>();
+            var idOptions = new IdentityOptions();
+            idOptions.Lockout.AllowedForNewUsers = false;
+
+            options.Setup(o => o.Value).Returns(idOptions);
+
+            var userValidators = new List<IUserValidator<TUser>>();
+
+            var validator = new Mock<IUserValidator<TUser>>();
+            userValidators.Add(validator.Object);
+            var pwdValidators = new List<PasswordValidator<TUser>>();
+            pwdValidators.Add(new PasswordValidator<TUser>());
+
+            var userManager = new UserManager<TUser>(store, options.Object, new PasswordHasher<TUser>(),
+                userValidators, pwdValidators, new UpperInvariantLookupNormalizer(),
+                new IdentityErrorDescriber(), null,
+                new Mock<ILogger<UserManager<TUser>>>().Object);
+
+            validator.Setup(v => v.ValidateAsync(userManager, It.IsAny<TUser>()))
+                .Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
+
+            return userManager;
+        }
+
+        private static SignInManager<TUser> SetupSignInManager<TUser>(UserManager<TUser> manager,
+            HttpContext context, ILogger logger = null, IdentityOptions identityOptions = null,
+            IAuthenticationSchemeProvider schemeProvider = null) where TUser : class
+        {
+            var contextAccessor = new Mock<IHttpContextAccessor>();
+            contextAccessor.Setup(a => a.HttpContext).Returns(context);
+            identityOptions = identityOptions ?? new IdentityOptions();
+            var options = new Mock<IOptions<IdentityOptions>>();
+            options.Setup(a => a.Value).Returns(identityOptions);
+            var claimsFactory = new UserClaimsPrincipalFactory<TUser>(manager, options.Object);
+            schemeProvider = schemeProvider ?? new Mock<IAuthenticationSchemeProvider>().Object;
+            var sm = new SignInManager<TUser>(manager, contextAccessor.Object, claimsFactory, options.Object, null, schemeProvider, new DefaultUserConfirmation<TUser>());
+            sm.Logger = logger ?? (new Mock<ILogger<SignInManager<TUser>>>()).Object;
+            return sm;
+        }
+    }
+}
---
+++ MovieTheater.Test/UnitTests/MoviesControllerTest.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MovieTheater.Controllers;
+using MovieTheater.DTOs;
+using MovieTheater.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTheater.Test.UnitTests
+{
+    [TestClass]
+    public class MoviesControllerTest : BaseTests
+    {
+        [TestMethod]
+        public async Task GetMoviesFilteredByTitle()
+        {
+            var dbName = await CreateTestData();
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+            
+            var controller = new MoviesController(context, mapper, null, null);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var movieFilterDTO = new MovieFilterDTO { Title = "Movie 1" };
+            var response = await controller.Filter(movieFilterDTO);
+            var results = response.Value;
+            
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(movieFilterDTO.Title, results[0].Title);
+        }
+
+        [TestMethod]
+        public async Task GetMoviesFilteredByAtCinema()
+        {
+            var dbName = await CreateTestData();
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+
+            var controller = new MoviesController(context, mapper, null, null);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var movieFilterDTO = new MovieFilterDTO { AtCinema = true };
+            var response = await controller.Filter(movieFilterDTO);
+            var results = response.Value;
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("Movie in theaters", results[0].Title);
+            Assert.IsTrue(results[0].AtCinema);
+        }
+
+        [TestMethod]
+        public async Task GetMoviesFilteredByNextPremiere()
+        {
+            var dbName = await CreateTestData();
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+
+            var controller = new MoviesController(context, mapper, null, null);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var movieFilterDTO = new MovieFilterDTO { NextPremier = true };
+            var response = await controller.Filter(movieFilterDTO);
+            var results = response.Value;
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("Not Premiered", results[0].Title);
+        }
+
+        [TestMethod]
+        public async Task GetMoviesFilteredByGender()
+        {
+            var dbName = await CreateTestData();
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+            var genderId = context.Genders.Select(g => g.Id).First();
+
+            var controller = new MoviesController(context, mapper, null, null);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var movieFilterDTO = new MovieFilterDTO { GenderId = genderId };
+            var response = await controller.Filter(movieFilterDTO);
+            var results = response.Value;
+
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("Movie with gender", results[0].Title);
+        }
+
+        [TestMethod]
+        public async Task GetMoviesFilteredOrderByTitleAscending()
+        {
+            var dbName = await CreateTestData();
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+
+            var controller = new MoviesController(context, mapper, null, null);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var movieFilterDTO = new MovieFilterDTO { OrderField = "Title", OrderByAsc = true };
+            var response = await controller.Filter(movieFilterDTO);
+            var results = response.Value;
+
+            var context2 = BuildContext(dbName);
+            var moviesDb = await context2.Movies.OrderBy(m => m.Title).ToListAsync();
+            Assert.AreEqual(moviesDb.Count, results.Count);
+            for (int i = 0; i < moviesDb.Count; i++)
+            {
+                Assert.AreEqual(moviesDb[i].Id, results[i].Id);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetMoviesFilteredOrderByTitleDescending()
+        {
+            var dbName = await CreateTestData();
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+
+            var controller = new MoviesController(context, mapper, null, null);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var movieFilterDTO = new MovieFilterDTO { OrderField = "Title", OrderByAsc = false };
+            var response = await controller.Filter(movieFilterDTO);
+            var results = response.Value;
+
+            var context2 = BuildContext(dbName);
+            var moviesDb = await context2.Movies.OrderByDescending(m => m.Title).ToListAsync();
+            Assert.AreEqual(moviesDb.Count, results.Count);
+            for (int i = 0; i < moviesDb.Count; i++)
+            {
+                Assert.AreEqual(moviesDb[i].Id, results[i].Id);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetMoviesFilteredOrderByWorngField ()
+        {
+            var dbName = await CreateTestData();
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+            var mock = new Mock<ILogger<MoviesController>>();
+
+            var controller = new MoviesController(context, mapper, null, mock.Object);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            var movieFilterDTO = new MovieFilterDTO { OrderField = "test", OrderByAsc = true };
+            var response = await controller.Filter(movieFilterDTO);
+            var results = response.Value;
+
+            var context2 = BuildContext(dbName);
+            var moviesDb = await context2.Movies.ToListAsync();
+            Assert.AreEqual(moviesDb.Count, results.Count);
+            Assert.AreEqual(1, mock.Invocations.Count);
+        }
+
+        private async Task<string> CreateTestData()
+        {
+            var dbName = Guid.NewGuid().ToString();
+            var context = BuildContext(dbName);
+            var gender = new Gender { Name = "Gender 1" };
+            var movies = new List<Movie>
+            {
+                new Movie { Title = "Movie 1", PremiereDate = new DateTime(2010,1,1), AtCinema = false },
+                new Movie { Title = "Not Premiered", PremiereDate = DateTime.Today.AddDays(2), AtCinema = false },
+                new Movie { Title = "Movie in theaters", PremiereDate = DateTime.Today.AddDays(-2), AtCinema = true },
+            };
+            var movieWithGender = new Movie
+            {
+                Title = "Movie with gender",
+                PremiereDate = new DateTime(2010, 1, 1),
+                AtCinema = false
+            };
+            movies.Add(movieWithGender);
+            
+            await context.Genders.AddAsync(gender);
+            await context.Movies.AddRangeAsync(movies);
+            await context.SaveChangesAsync();
+            await context.MovieGenders.AddAsync(new MovieGender { GenderId = gender.Id, MovieId = movieWithGender.Id });
+            await context.SaveChangesAsync();
+            
+            return dbName;
+        }
+    }
+}
