da6901d832890b374834be1fe58ffde29d2f94a9
NeshGogo <rafaelaguero10@hotmail.com>
2021-06-13 14:36:20 -0400

    Creating test to ReviewsController.

--- MovieTheater.Test/BaseTests.cs
+++ MovieTheater.Test/BaseTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieTheater.Helpers;
 using NetTopologySuite;
@@ -10,6 +13,9 @@ namespace MovieTheater.Test
 {
     public class BaseTests
     {
+        protected readonly string defaultUserId = "52736a28-633f-496c-9c2e-3d1fb986a9fd";
+        protected readonly string defaultUserEmail = "userTest@test.com";
+
         protected MovieTheaterDbContext BuildContext(string dbName)
         {
             var options = new DbContextOptionsBuilder<MovieTheaterDbContext>()
@@ -25,6 +31,21 @@ namespace MovieTheater.Test
                options.AddProfile(new AutoMapperProfiles(geometryFactory));
            });
             return config.CreateMapper();
+        }
+
+        protected ControllerContext BuildControllerContext()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Email, defaultUserEmail),
+                new Claim(ClaimTypes.Name, defaultUserEmail),
+                new Claim(ClaimTypes.NameIdentifier, defaultUserId),
+            }));
+
+            return new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
         }
     }
 }
---
+++ MovieTheater.Test/UnitTests/ReviewsControllerTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieTheater.Controllers;
+using MovieTheater.DTOs;
+using MovieTheater.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTheater.Test.UnitTests
+{
+    [TestClass]
+    public class ReviewsControllerTest : BaseTests
+    {
+        [TestMethod]
+        public async Task UserCannotAddTwoReviewsToTheSameMovie()
+        {
+            var dbName = Guid.NewGuid().ToString();
+            await InitData(dbName);
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+            var movieId = context.Movies.Select(m => m.Id).First();
+            var review = new Review { MovieId = movieId, Score = 5, UserId = defaultUserId, Comment = "good" };
+            await context.Reviews.AddAsync(review);
+            await context.SaveChangesAsync();
+            var context2 = BuildContext(dbName);
+            var controller = new ReviewsController(context2, mapper);
+            controller.ControllerContext = BuildControllerContext();
+            var reviewCreate = new ReviewCreateDTO { Score = 5, Comment = "Great" };
+            var response = await controller.Post(movieId, reviewCreate);
+            var result = response as BadRequestObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(StatusCodes.Status400BadRequest, result.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task CreateReview()
+        {
+            var dbName = Guid.NewGuid().ToString();
+            await InitData(dbName);
+            var context = BuildContext(dbName);
+            var mapper = ConfigureAutoMapper();
+            var movieId = context.Movies.Select(m => m.Id).First();
+            var controller = new ReviewsController(context, mapper);
+            controller.ControllerContext = BuildControllerContext();
+            var reviewCreate = new ReviewCreateDTO { Score = 5, Comment = "Great" };
+            var response = await controller.Post(movieId, reviewCreate);
+            var result = response as StatusCodeResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(StatusCodes.Status204NoContent, result.StatusCode);
+            var context2 = BuildContext(dbName);
+            var review = await context2.Reviews.FirstOrDefaultAsync();
+            Assert.IsNotNull(review);
+            Assert.AreEqual(defaultUserId, review.UserId);
+        }
+
+        private async Task InitData(string dbName)
+        {
+            var context = BuildContext(dbName);
+            await context.Movies.AddAsync(new Movie
+            {
+                Title = "Emma",
+                AtCinema = false,
+                PremiereDate = new DateTime(2020, 02, 21)
+            });
+            await context.SaveChangesAsync();
+        }
+    }
+}
