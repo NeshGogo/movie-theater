38843bef1d97941e0d9b9bd15c13ca7417acb3cd
NeshGogo <rafaelaguero10@hotmail.com>
2021-06-19 17:51:15 -0400

    Initialization to work with integration tests.

--- MovieTheater.Test/BaseTests.cs
+++ MovieTheater.Test/BaseTests.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using MovieTheater.Helpers;
 using NetTopologySuite;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MovieTheater.Test
 {
@@ -46,6 +51,32 @@ namespace MovieTheater.Test
             {
                 HttpContext = new DefaultHttpContext { User = user }
             };
+        }
+
+        protected WebApplicationFactory<Startup> BuildWebApplicationFactory(string dbName, bool securityIgnore = true)
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            
+            factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    var descriptorDbContext = services.SingleOrDefault(d =>
+                   d.ServiceType == typeof(DbContextOptions<MovieTheaterDbContext>));
+                    if (descriptorDbContext != null) services.Remove(descriptorDbContext);
+
+                    if (!securityIgnore)
+                    {
+                       services.AddSingleton<IAuthorizationHandler, AllowAnonymousHandler>();
+                       services.AddControllers(options =>
+                       {
+                           options.Filters.Add(new FakeUserFilter());
+                       });
+                    }
+                });
+            });
+
+            return factory;
         }
     }
 }
---
+++ MovieTheater.Test/FakeUserFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTheater.Test
+{
+    class FakeUserFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            context.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Email, "userTest@test.com"),
+                new Claim(ClaimTypes.Name, "userTest@test.com"),
+                new Claim(ClaimTypes.NameIdentifier, "52736a28-633f-496c-9c2e-3d1fb986a9fd"),
+            }));
+
+            await next();
+        }
+    }
+}
